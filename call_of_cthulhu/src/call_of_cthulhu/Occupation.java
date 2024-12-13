9bf908454635ac50bc31c972bbf26b462d151d99
Finch McCambridge <finchmccambridge01@gmail.com>
2024-12-13 12:33:59 -0500

    Change occupations
    Same as skills, need to delete old file when I get the change

---
+++ call_of_cthulhu/src/call_of_cthulhu/Occupation.java
@@ -0,0 +1,15 @@
+package call_of_cthulhu;
+
+public class Occupation {
+	private int osp;
+	private int crMin;
+	private int crMax;
+	private String name;
+	
+	public Occupation(int osp, int crMin, int crMax, String name) {
+		this.osp = osp;
+		this.crMin = crMin;
+		this.crMax = crMax;
+		this.name = name;
+	}
+}
